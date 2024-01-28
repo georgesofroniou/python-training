1f463dcd9079dd912c774cbbdba1a6fc3ee55a4b
georgesofroniou <georgesofroniou@gmail.com>
2024-01-28 21:46:28 +0000

    G-20240126 Initial Commit Lesson 1 (Getting Started), 2 (Python Basics) & 3 (Operators)

---
+++ lesson-02/welcome.py
@@ -0,0 +1,12 @@
+# You make comments by giving # then type your comment
+line01 = "********************"  # header / footer
+line02 = "*                  *"  # re-use
+line03 = "*     WELCOME!     *"
+
+# starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
